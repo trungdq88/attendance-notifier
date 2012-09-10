17399584f72697b76efb70fad42a6fead482859b
trungdqse60994@fpt.edu.vn <trungdqse60994@fpt.edu.vn@282c95b9-3c2d-ed7e-042c-da8c65c47596>
2012-09-10 02:44:09 +0000

    Commit lần đầu.

---
+++ func.inc.php
@@ -0,0 +1,58 @@
+<?php
+
+function checkLogin($username, $password) {
+	$post_data = array('username'=>$username, 'password'=>$password, 'testcookies'=>'0', 'iboard_url'=>''); 
+	$ckfile = tempnam("/tmp", "CURLCOOKIE");
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, "http://cms-hcm.fpt.edu.vn/elearning/login/index.php"); 
+	curl_setopt($ch, CURLOPT_POST, 1); 
+	curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
+	curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); 
+	$index = curl_exec($ch); 
+	curl_close($ch);
+	//$index = "You are logged in as <a href=\"http://cms-hcm.fpt.edu.vn/elearning/user/view.php?id=2292&amp;course=1\">Trung Đinh Quang</a>";
+	
+	if (preg_match("/(?<=You are logged in as ).*?<\/a\>/i", $index, $html_name)) {
+		preg_match("/(?<=>).*?(?=<)/i", $html_name[0], $name);
+		//echo ($name[0]);
+		return $name[0];
+	} else {
+		return false;
+	}
+}
+
+function checkAttendance($username, $password, $arrID) {
+	$post_data = array('username'=>$username, 'password'=>$password, 'testcookies'=>'0', 'iboard_url'=>''); 
+	$ckfile = tempnam("/tmp", "CURLCOOKIE");
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, "http://cms-hcm.fpt.edu.vn/elearning/login/index.php"); 
+	curl_setopt($ch, CURLOPT_POST, 1); 
+	curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
+	curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data); 
+	$index = curl_exec($ch); 
+	curl_close($ch);
+	//echo $index;
+	foreach ($arrID as $id) {
+		$ch = curl_init(); 
+		curl_setopt($ch, CURLOPT_URL, 'http://cms-hcm.fpt.edu.vn/elearning/mod/attforblock/view.php?id='.$id); 
+		curl_setopt($ch, CURLOPT_COOKIEFILE, $ckfile);
+		curl_setopt($ch, CURLOPT_COOKIEJAR, $ckfile);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+		$pm = curl_exec($ch);
+		curl_close($ch);
+		//echo $pm;
+		if ($pm) {
+			//Get info
+			//Return info
+		} else {
+			return false;
+		}
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,66 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+    <meta name="author" content="nhannvse60650"/>
+    <link rel="stylesheet" type="text/css" href="css/reset.css" />
+    <link rel="stylesheet" type="text/css" href="css/elastic.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/elastic.js"></script>
+    <title>Front End</title>
+</head>
+<body>
+    <div id="wrapper" class="container">
+        <div class="columns unit">
+            <div id="header" class="column elastic">
+                <h1><a href="#">Logo</a></h1>
+            </div><!--end .container-->
+        </div>
+        <div id="content-wrap" >
+            <div id="content" class="unit">
+                <div class="columns same-height">
+
+                    <div id="main-content" class="column elastic">
+                        <div id="welcome">
+                            <h1>Chào mừng các bạn đến với Attendance Checker</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu neque in massa tristique aliquet fringilla a libero.
+                                Vestibulum porttitor ligula ut tellus vestibulum pharetra. Donec luctus dui et eros vehicula sed ullamcorper enim vehicula.
+                                Maecenas dapibus elit in odio elementum non fringilla felis tincidunt. Donec pretium pretium consectetur.
+                            </p>
+                        </div><!--end #welcome-->
+                    </div><!--end #main-content-->
+                    <div id="sidebar" class="column fixed equal-height" style="width:335px;">
+                        <div id="login-form">
+                            <h1>Đăng Nhập</h1>
+                            <form action="" method="POST">
+                                <p>
+                                    <label for="email">Email:</label>
+                                    <input type="text" name="email" id="email" />
+                                </p>
+                                <p>
+                                    <label for="password">Password:</label>
+                                    <input type="text" name="password" id="password" />
+                                </p>
+                                <p>
+                                    <input type="submit" name="submit" value="Send" />
+                                </p>
+                            </form><!--end form-->
+                        </div><!--end login-form-->
+                    </div><!--end #sidebar-->
+                </div>
+            </div><!--end #content-->
+        </div>
+
+        <div id="footer" class="columns unit">
+            <div class="elastic">
+                <p>&copy; 2012 | Attendance Checker</p>
+            </div>
+        </div><!--end footer-->
+        <div class="clear"></div>
+    </div><!--end #wrapper-->
+</body>
+</html>
