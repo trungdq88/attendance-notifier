6699a618380e3cc2955ed749b50bc0d160df1a77
trungdqse60994@fpt.edu.vn <trungdqse60994@fpt.edu.vn@282c95b9-3c2d-ed7e-042c-da8c65c47596>
2012-09-14 12:17:40 +0000

    Test online gửi mail + check absent thành công. Cần data về id của các môn để hoàn thành CSDL.

--- sendmail.php
+++ sendmail.php
@@ -10,7 +10,7 @@
 		$mailId = $row['ID'];
 		$sendTo = $row['SendTo'];
 		$name = $row['Name'];
-		$subject = $row['Subject'];
+		$subject = $row['Subject']; //TODO: get subject name by id
 		$absent = $row['Absent'];
 		$percent = $row['Percent'];
 		
@@ -18,17 +18,12 @@
 		<html>
 		<head>
 		<title>Attendance Notifier</title>
-		<style type= 'text/css '>
-		.das {
-			color: #F00;
-		}
-		</style>
 		</head>
 		<body>
 		<h2>Chào bạn <strong>$name</strong>!</h2>
-		<p>Bạn vừa vắng mặt một slot môn <strong class= 'das '>$subject</strong> vào ngày hôm nay.</p>
-		<h3>Số slot Absent: <span class= 'das '>$absent</span></h3>
-		<h3>Số phần trăm nghỉ: <span class= 'das '>$percent%</span> <em class= 'das '>(trên 20% sẽ bị cấm thi)</em></h3>
+		<p>Bạn vừa vắng mặt một slot môn <strong style='color:red'>$subject</strong> vào ngày hôm nay.</p>
+		<h3>Số slot Absent: <span style='color:red'>$absent</span></h3>
+		<h3>Số phần trăm nghỉ: <span style='color:red'>$percent%</span> <em style='color:red'>(trên 20% sẽ bị cấm thi)</em></h3>
 		<br />
 		<p><em>Email này được gửi từ dịch vụ Attendance Notifier, cảm ơn bạn đã sử dụng dịch vụ.</em><br/>
 		<a href= 'http://full.name.vn/att'>http://full.name.vn/att</a></p>
