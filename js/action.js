b9bae263dc4cc25e8dc2156add3efbc49608673d
trungdqse60994@fpt.edu.vn <trungdqse60994@fpt.edu.vn@282c95b9-3c2d-ed7e-042c-da8c65c47596>
2012-09-15 16:15:23 +0000

    Ajax loading ok. Online tested. Next: nội dung.

--- js/action.js
+++ js/action.js
@@ -2,13 +2,13 @@ $(document).ready(function(e) {
 	$('#userpanel').hide(0);
     $('#btnLogin').click(function(e) {
 		waitting(false);
-		showStatus("Đang kết nối...");
+		showStatus("Đang kết nối...", true);
         var username = $('#txtUsername').val();
 		var password = $('#txtPassword').val();
 		var data = {'do':'login',
 					'username':username,
 					'password':password};
-		showStatus("Kết nối đến CMS...");
+		showStatus("Kết nối đến CMS...", true);
 		$.ajax({
 			type: "POST",
 			url: "control.php",
@@ -18,9 +18,10 @@ $(document).ready(function(e) {
 			dataType: "json",
 			timeout: 20000, // in milliseconds
 			success: function(d) {
-				showStatus("Đang đăng nhập...");
+				showStatus("Đang đăng nhập...", true);
+				waitting(true);
 				if (d) {
-					showStatus("Đăng nhập thành công!");
+					showStatus("Đăng nhập thành công!", false);
 					//Login successful
 					var name = d['Name'];
 					var sess = d['Session'];
@@ -30,33 +31,47 @@ $(document).ready(function(e) {
 					switchToUserPanel(name, sess, subjectIds, email, emailFreq);
 				} else {
 					//Login failed
-					showStatus("Sai mật khẩu!");
+					showStatus("Sai mật khẩu!", false);
 				}
-				waitting(true);
 			},
 			error: function(request, status, err) {
-				showStatus("Thất bại: " + status + " (" + err + ")");
+				waitting(true);
+				showStatus("Thất bại: " + status + " (" + err + ")", false);
 				if(status == "timeout") {
 					
 				}
-				waitting(true);
 			}
 		});
     });
 	$('#save-button').click(function(e) {
-		var data = {'do':'savesetting',
+		$('#save-button').hide();
+		$.ajax({
+			type: "POST",
+			url: "control.php",
+			data: {'do':'savesetting',
 					'username':$('#lblUsername').html(),
 					'session': $('#lblSession').html(),
 					'subjectids':getSubjectIds(),
 					'email':$('#email').val(),
-					'emailfreq':$('#frequen').val()};
-		$.post('control.php', data, function(d) {
-			if (d == "YES") {
-				alert("Các thay đổi đã được lưu thành công.");
-			} else {
-				alert("Có lỗi xảy ra:\n\n" + d);
+					'emailfreq':$('#frequen').val()},
+			timeout: 10000, // in milliseconds
+			success: function(d) {
+				if (d == "YES") {
+					alert("Các thay đổi đã được lưu thành công.");
+				} else {
+					alert("Có lỗi xảy ra:\n\n" + d);
+				}
+				$('#save-button').fadeIn(300);
+			},
+			error: function(request, status, err) {
+				alert("Có lỗi xảy ra: " + err + "\nXin thử lại!");
+				$('#save-button').fadeIn(300);
 			}
 		});
+	});
+	
+	$('#stop-service').click(function(e){
+		document.location.reload(true);
 	});
 });
 
@@ -87,17 +102,22 @@ function loadSubjectIds(ids) {
 	}
 }
 
-function showStatus(status) {
+function showStatus(status, loading) {
 	$('#status').html(status);
+	if (loading) {
+		$('#status-image').fadeIn(200);
+	} else {
+		$('#status-image').fadeOut(200);
+	}
 }
 
 function waitting(end){
 	if (end) {
-		$('#btnLogin').removeAttr("class");
+		$('#btnLogin').fadeIn(300);
 		$('#txtUsername').removeAttr("disabled");
 		$('#txtPassword').removeAttr("disabled");
 	} else {
-		$('#btnLogin').attr("class","disabled");
+		$('#btnLogin').fadeOut(300);
 		$('#txtUsername').attr("disabled","disabled");
 		$('#txtPassword').attr("disabled","disabled");
 	}
